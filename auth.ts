1840c047ba570a47e8c11ed2e8302eabfb27d36e
mughilan16 <mughilant@proton.me>
2025-01-01 15:46:17 +0530

    add auth route

--- auth.ts
+++ auth.ts
@@ -1,3 +1,5 @@
+import { login } from "@/api/user";
+import axios from "axios";
 import { AuthOptions } from "next-auth";
 import { getServerSession } from "next-auth/next";
 import Credentials from "next-auth/providers/credentials";
@@ -7,12 +9,12 @@ const authOptions: AuthOptions = {
         Credentials({
             name: "Credentials",
             credentials: {
-                username: { label: "Username", type: "text" },
-                Password: { label: "Password", type: "password" }
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
             },
             async authorize(credentials, req) {
-                console.log(credentials, req);
-                const user = { id: "1", name: "J Smith", email: "email@gmail.com" };
+                const user = login({email: credentials?.email!, password: credentials?.password!}).then(data => data.user);
+                //const user = { id: "1", name: "J Smith", email: "email@gmail.com" };
                 if (user) {
                     return user
                 } else {
